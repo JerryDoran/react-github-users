2957db1d111a0ba9987b80db7dd0a18a0ececd11
Jerome Doran <disneyfan9184@gmail.com>
2020-10-11 13:54:12 -0400

    Added authentication with Auth0

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+// dev-o6vgn7o9.us.auth0.com
+// PXOTfa1K1C8FQjeCVVbeHngMIfBDl7od
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-o6vgn7o9.us.auth0.com'
+      clientId='PXOTfa1K1C8FQjeCVVbeHngMIfBDl7od'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
